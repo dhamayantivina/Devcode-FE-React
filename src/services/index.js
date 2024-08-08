6ba9391671f2479cdda0a852f8b365330fde1861
dhamayantivina <dhamayantivina1@gmail.com>
2024-08-08 22:43:55 +0700

    FE-React-Level-3

---
+++ src/services/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.contact-manager.project.skyshi.io";
+
+export async function getAllContactsData() {
+  return await axios.get(`${BASE_URL}/contacts`).catch((error) => {
+    return error;
+  });
+}
+
+// Uncomment this function to add a new contact
+export async function addNewContact(payload) {
+  return await axios.post(`${BASE_URL}/contacts`, payload).catch((error) => {
+    return error;
+  });
+}
